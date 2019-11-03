206f0920237a6009f0ac26e93144be4f32b04dcd
Nitish <nitishtalekar.nt503@gmail.com>
2019-11-03 13:39:49 +0530

    pivt update

--- Common/Projects/Pictionary/games/pictionary/views.py
+++ Common/Projects/Pictionary/games/pictionary/views.py
@@ -7,35 +7,41 @@ from . import views
 # Create your views here.
 def home(request,k):
     p = request.session['players']
+    n = request.session['nop']
+    t = request.session['turn']
     if request.method == "POST":
         form = Btnform(request.POST)
         form2 = Rollform(request.POST)
         if form.is_valid():
             if 'correct' in request.POST:
-                request.session['score'] = request.session['score']+1
+                request.session['score'][t] = request.session['score'][t]+request.session['dice']
                 request.session['dice'] = 0
+                request.session['turn'] = (request.session['turn'] + 1)%n
             elif 'wrong' in request.POST:
-                request.session['score'] = request.session['score']
+                request.session['score'][t] = request.session['score'][t]
                 request.session['dice'] = 0
+                request.session['turn'] = (request.session['turn'] + 1)%n
         if form2.is_valid():
             if 'roll' in request.POST:
                 request.session['dice'] = r.choice([i for i in range(1,7)])
+                request.session['turn'] = request.session['turn']
         dice = request.session['dice']
         score = request.session['score']
     else:
         form = Btnform()
         form2 = Rollform()
-        request.session['score'] = 0
+        request.session['score'] = [0 for i in range(request.session['nop'])]
         score = request.session['score']
         request.session['dice'] = 0
         dice = request.session['dice']
-    return render(request,'pictionary/home.html',{'dice':dice,'score':score,'form':form,'form2':form2,'players':p})
+    t = request.session['turn']
+    return render(request,'pictionary/home.html',{'dice':dice,'score':score,'form':form,'form2':form2,'players':p,'nop':n,'turn':t})
     
 def login(request):
     if request.method == "POST":
         login = Loginform(request.POST)
         players = []
-        ind = 1
+        ind = 0
         if login.is_valid():
             if login.cleaned_data['team1']:
                 players.append((ind , login.cleaned_data['team1'],login.cleaned_data['color1']))
@@ -50,7 +56,10 @@ def login(request):
                 players.append((ind , login.cleaned_data['team4'],login.cleaned_data['color4']))
                 ind = ind+1
             request.session['players'] = players
-            request.session['score'] = 0
+            nop = len(players)
+            request.session['nop'] = nop
+            request.session['score'] = [0 for i in range(request.session['nop'])]
+            request.session['turn'] = 0
             request.session['dice'] = 0
         return home(request,0)
     else:        
