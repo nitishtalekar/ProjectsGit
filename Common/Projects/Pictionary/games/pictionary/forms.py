ba55328bd0c85ae229b19c443cf75130296fcd16
Nitish <nitishtalekar.nt503@gmail.com>
2019-11-04 06:33:30 +0530

    LOGIN PAGE

--- Common/Projects/Pictionary/games/pictionary/forms.py
+++ Common/Projects/Pictionary/games/pictionary/forms.py
@@ -11,7 +11,7 @@ class Cardform(forms.Form):
     check3 = forms.CharField(widget = forms.HiddenInput(),required=False)
 
 class Loginform(forms.Form):
-    team1 = forms.CharField(required=False,max_length=20)
-    team2 = forms.CharField(required=False,max_length=20)
-    team3 = forms.CharField(required=False,max_length=20)
-    team4 = forms.CharField(required=False,max_length=20)
+    team1 = forms.CharField(required=False,max_length=20,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    team2 = forms.CharField(required=False,max_length=20,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    team3 = forms.CharField(required=False,max_length=20,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    team4 = forms.CharField(required=False,max_length=20,widget=forms.TextInput(attrs={'class': 'form-control'}))
