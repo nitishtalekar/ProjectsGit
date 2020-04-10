f32df4cb67f9d17b91aa64ae3564fbb9f266e3cb
Nitish <nitishtalekar.nt503@gmail.com>
2020-04-10 16:40:31 +0530

    donendusted

---
+++ Common/Last FB/FeedbackNew/include/header.php
@@ -0,0 +1,19 @@
+<style media="screen">
+  .admin_nav{
+    text-transform: uppercase;
+    font-family: Montserrat-SemiBold;
+  }
+</style>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand admin_nav" href="/FeedbackNew/admin">Admin</a>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link admin_nav" href="/FeedbackNew/admin/admin.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link admin_nav" href="/FeedbackNew/">Feedback</a>
+      </li>
+    </ul>
+  </div>
+</nav>
