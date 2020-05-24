44df6804536c7836b64316602511317c533d4726
Nitish <nitishtalekar.nt503@gmail.com>
2020-05-24 22:21:59 +0530

    FE

---
+++ Common/Ongoing/Pictionary/New FrontEnd/Pictionary/board.js
@@ -0,0 +1,25 @@
+
+  
+  
+
+// function AniDice(x)
+// {
+//   MyVar=setInterval(rolldice,75);
+// 
+//   var delayInMilliseconds = x*500; //1 second
+// 
+//   setTimeout(function() {
+//       clearInterval(MyVar);
+//       // document.getElementById("dice").innerHTML = 11;
+//       document.getElementById("diceimg").setAttribute("src", 'img/dice1b.png');
+//   }, delayInMilliseconds);
+// 
+// }
+// function rolldice()
+// {
+// var ranNum = Math.floor( 1 + Math.random() * 6 );
+// var dice = ['img/dice1b.png','img/dice2b.png','img/dice3b.png','img/dice4b.png','img/dice5b.png','img/dice6b.png']
+// // document.getElementById("dice").innerHTML = ranNum;
+// document.getElementById("diceimg").setAttribute("src", dice[ranNum-1]);
+// 
+// }
