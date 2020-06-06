767d7849416ef3f323b26d08a1b84bfcb9c3a6ec
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2020-06-07 01:19:25 +0530

    done

--- Common/Ongoing/Calculator/main.py
+++ Common/Ongoing/Calculator/main.py
@@ -11,29 +11,157 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
- 
 
-class Interface(Widget):  
-    name = ObjectProperty(None)
-    lname = ObjectProperty(None)
-    email = ObjectProperty(None)
-    inputs = ObjectProperty(None)
-    
-    def submit(self):
-        print("Name: ", self.name.text)
-        print("Last Name: ", self.lname.text)
-        print("Email: ", self.email.text)
-        self.inputs.text = "Your IP: " + self.name.text + " - " + self.lname.text + " - " + self.email.text
-        self.name.text = ""
-        self.lname.text = ""
-        self.email.text = ""
-        
-    
-class CalculatorApp(MDApp):    
+
+def precedence(op):
+
+    if op == '+' or op == '-':
+        return 1
+    if op == '*' or op == '/' or op == '%':
+        return 2
+    return 0
+
+# Function to perform arithmetic
+# operations.
+def applyOp(a, b, op):
+
+    if op == '+': return a + b
+    if op == '-': return a - b
+    if op == '*': return a * b
+    if op == '/': return a // b
+    if op == '%': return a % b
+
+# Function that returns value of
+# expression after evaluation.
+def evaluate(tokens):
+
+    # stack to store integer values.
+    values = []
+
+    # stack to store operators.
+    ops = []
+    i = 0
+
+    while i < len(tokens):
+
+        # Current token is a whitespace,
+        # skip it.
+        if tokens[i] == ' ':
+            i += 1
+            continue
+
+        # Current token is an opening
+        # brace, push it to 'ops'
+        elif tokens[i] == '(':
+            ops.append(tokens[i])
+
+        # Current token is a number, push
+        # it to stack for numbers.
+        elif tokens[i].isdigit():
+            val = 0
+
+            # There may be more than one
+            # digits in the number.
+            while (i < len(tokens) and
+                tokens[i].isdigit()):
+
+                val = (val * 10) + int(tokens[i])
+                i += 1
+
+            values.append(val)
+
+        # Closing brace encountered,
+        # solve entire brace.
+        elif tokens[i] == ')':
+
+            while len(ops) != 0 and ops[-1] != '(':
+
+                val2 = values.pop()
+                val1 = values.pop()
+                op = ops.pop()
+
+                values.append(applyOp(val1, val2, op))
+
+            # pop opening brace.
+            ops.pop()
+
+        # Current token is an operator.
+        else:
+
+            # While top of 'ops' has same or
+            # greater precedence to current
+            # token, which is an operator.
+            # Apply operator on top of 'ops'
+            # to top two elements in values stack.
+            while (len(ops) != 0 and
+                precedence(ops[-1]) >= precedence(tokens[i])):
+
+                val2 = values.pop()
+                val1 = values.pop()
+                op = ops.pop()
+
+                values.append(applyOp(val1, val2, op))
+
+            # Push current token to 'ops'.
+            ops.append(tokens[i])
+
+        i += 1
+
+    # Entire expression has been parsed
+    # at this point, apply remaining ops
+    # to remaining values.
+    while len(ops) != 0:
+
+        val2 = values.pop()
+        val1 = values.pop()
+        op = ops.pop()
+
+        values.append(applyOp(val1, val2, op))
+
+    # Top of 'values' contains result,
+    # return it.
+    return values[-1]
+
+
+
+
+
+
+class Interface(Widget):
+    # name = ObjectProperty(None)
+    # lname = ObjectProperty(None)
+    # email = ObjectProperty(None)
+    # inputs = ObjectProperty(None)
+    num = ObjectProperty(None)
+    display = ObjectProperty(None)
+
+    def submit(self, text):
+        # print("Name: ", self.name.text)
+        # print("Last Name: ", self.lname.text)
+        # print("Email: ", self.email.text)
+        # self.inputs.text = "Your IP: " + self.name.text + " - " + self.lname.text + " - " + self.email.text
+        # self.name.text = ""
+        # self.lname.text = ""
+        # self.email.text = ""
+        # print(text)
+        # print("\n hi",self.display.text)
+        if self.display.text == "0":
+            self.display.text = ""
+        if text == "=":
+            # print(self.display.text)
+            # print(evaluate(self.display.text))
+            self.display.text = str(evaluate(self.display.text))
+            text = ""
+        self.display.text = self.display.text + text
+        # print("\n yo", self.display.text)
+        if text == "CLEAR":
+            self.display.text = ""
+
+
+class CalculatorApp(MDApp):
     def build(self):
         return Interface()
 
 
 if __name__ == "__main__":
     CalculatorApp().run()
-        
