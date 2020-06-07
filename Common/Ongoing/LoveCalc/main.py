4952e0d6c688a4ca32fdb229db49d693a8d12dd3
Nitish <nitishtalekar.nt503@gmail.com>
2020-06-08 01:08:45 +0530

    lovec

--- Common/Ongoing/LoveCalc/main.py
+++ Common/Ongoing/LoveCalc/main.py
@@ -2,6 +2,7 @@ import kivy
 import kivymd
 
 from kivymd.app import MDApp
+from kivymd.theming import ThemeManager
 from kivy.lang import Builder
 
 from kivy.app import App
@@ -11,27 +12,42 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
  
 
-class Interface(Widget):  
-    name = ObjectProperty(None)
-    lname = ObjectProperty(None)
-    email = ObjectProperty(None)
-    inputs = ObjectProperty(None)
+# class Interface(Widget):  
+#     name = ObjectProperty(None)
+#     lname = ObjectProperty(None)
+#     email = ObjectProperty(None)
+#     inputs = ObjectProperty(None)
+# 
+#     def submit(self):
+#         print("Name: ", self.name.text)
+#         print("Last Name: ", self.lname.text)
+#         print("Email: ", self.email.text)
+#         self.inputs.text = "Your IP: " + self.name.text + " - " + self.lname.text + " - " + self.email.text
+#         self.name.text = ""
+#         self.lname.text = ""
+#         self.email.text = ""
+
+class HomeScreen(Screen):
+    pass
+
+class AnsScreen(Screen):
+    pass
     
-    def submit(self):
-        print("Name: ", self.name.text)
-        print("Last Name: ", self.lname.text)
-        print("Email: ", self.email.text)
-        self.inputs.text = "Your IP: " + self.name.text + " - " + self.lname.text + " - " + self.email.text
-        self.name.text = ""
-        self.lname.text = ""
-        self.email.text = ""
+class CalcScreen(Screen):
+    pass
         
-    
-class LoveCalcApp(MDApp):    
+class Manager(ScreenManager):
+    pass
+        
+# kv = Builder.load_file("lovecalc.kv")    
+
+class LoveCalcApp(MDApp):  
+    # theme_cls = ThemeManager()  
     def build(self):
-        return Interface()
+        return
 
 
 if __name__ == "__main__":
