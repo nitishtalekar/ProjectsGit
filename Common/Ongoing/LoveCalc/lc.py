45616a341592d0f3c1b605cec61091c58d616bbd
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2020-06-08 02:29:57 +0530

    lcalc

--- Common/Ongoing/LoveCalc/lc.py
+++ Common/Ongoing/LoveCalc/lc.py
@@ -1,20 +1,23 @@
 
 
-def lc(s):
+
+def lcalc(s):
     sum = 0
-    print(s)
     if len(s) == 1:
         return s
     elif len(s) == 0:
         return ""
-    print(s[0],s[-1])
-    sum = str(int(s[0]) + int(s[-1])) + str(lc(s[1:-1]))
-    print(sum)
+    sum = str(int(s[0]) + int(s[-1])) + str(lcalc(s[1:-1]))
     return sum
 
+def score(s):
+    while len(s) > 2:
+        s = str(lcalc(s))
+    return s
 
 s = input("Enter string of no: ")
-while len(s) > 2:
-    s = str(lc(s))
-    print(s)
-print(s)
+# print(score(s))
+x = list(s)
+res = []
+[res.append(x) for x in x if x not in res]
+print(res)
--- Common/Ongoing/LoveCalc/main.py
+++ Common/Ongoing/LoveCalc/main.py
@@ -13,14 +13,14 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
- 
 
-# class Interface(Widget):  
+
+# class Interface(Widget):
 #     name = ObjectProperty(None)
 #     lname = ObjectProperty(None)
 #     email = ObjectProperty(None)
 #     inputs = ObjectProperty(None)
-# 
+#
 #     def submit(self):
 #         print("Name: ", self.name.text)
 #         print("Last Name: ", self.lname.text)
@@ -30,26 +30,67 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 #         self.lname.text = ""
 #         self.email.text = ""
 
+def lcalc(s):
+    sum = 0
+    if len(s) == 1:
+        return s
+    elif len(s) == 0:
+        return ""
+    sum = str(int(s[0]) + int(s[-1])) + str(lcalc(s[1:-1]))
+    return sum
+
+def score(s):
+    while len(s) > 2:
+        s = str(lcalc(s))
+    return s
+
+def lstring(s):
+    y = list(s)
+    res = []
+    num = ""
+    [res.append(x) for x in y if x not in res]
+    for i in res:
+        num = num + str(y.count(i))
+
+    return score(num)
+
+name = ""
 class HomeScreen(Screen):
-    pass
+    name1 = ObjectProperty(None)
+    name2 = ObjectProperty(None)
+    choice = ObjectProperty(None)
+    def submit(self,c):
+        global name
+        if c == True:
+            name = self.name1.text + "loves" + self.name2.text
+            name = lstring("".join(name.split(" ")).lower())
+        else:
+            name = self.name1.text + "friends" + self.name2.text
+            name = lstring("".join(name.split(" ")).lower())
+
+        print(name)
+        # h = AnsScreen()
+        # sub = h.ans
+        # sub(name)
+        self.manager.get_screen('ans').disp.text = name + "%"
 
 class AnsScreen(Screen):
-    pass
-    
+    disp = ObjectProperty(None)
+
+
 class CalcScreen(Screen):
     pass
-        
+
 class Manager(ScreenManager):
     pass
-        
-# kv = Builder.load_file("lovecalc.kv")    
 
-class LoveCalcApp(MDApp):  
-    # theme_cls = ThemeManager()  
+# kv = Builder.load_file("lovecalc.kv")
+
+class LoveCalcApp(MDApp):
+    # theme_cls = ThemeManager()
     def build(self):
         return
 
 
 if __name__ == "__main__":
     LoveCalcApp().run()
-        
