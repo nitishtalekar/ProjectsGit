17c8e4ef59e6b692e196f094c9f4e533d3a0f6ae
Nitish <nitishtalekar.nt503@gmail.com>
2020-07-27 23:55:34 +0530

    change

--- OngoingProjects/SnapinsightAdmin/SnapinsightAdmin/urls.py
+++ OngoingProjects/SnapinsightAdmin/SnapinsightAdmin/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snapinsight/',include("snapinsight.urls")),
+    path('',include("snapinsight.urls")),
 ]
