02539bc2c9723f723894926a02a71950b6119884
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2020-07-28 01:47:31 +0530

    :( snapinsight

--- OngoingProjects/SnapinsightAdmin/snapinsight/urls.py
+++ OngoingProjects/SnapinsightAdmin/snapinsight/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('overview/',views.overview),
+    path('solutions/',views.solutions),
+    path('games/',views.games),
+    path('services/',views.services),
+    path('legal/',views.legal),
+    path('ideas/',views.ideas),
+    path('moodish/overview/',views.moodish_overview),
+    path('moodish/projects/',views.moodish_projects),
+    path('moodish/resources/',views.moodish_resources),
+    path('moodish/roadmap/',views.moodish_roadmap),
+    path('moodish/tasks/',views.moodish_tasks),
 ]
