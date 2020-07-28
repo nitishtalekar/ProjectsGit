4e9891b044b13180a95c9a5b6a6b12d9163067b3
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2020-07-29 03:31:09 +0530

    snapinsight

--- OngoingProjects/SnapinsightAdmin/snapinsight/urls.py
+++ OngoingProjects/SnapinsightAdmin/snapinsight/urls.py
@@ -9,9 +9,26 @@ urlpatterns = [
     path('services/',views.services),
     path('legal/',views.legal),
     path('ideas/',views.ideas),
+
     path('moodish/overview/',views.moodish_overview),
     path('moodish/projects/',views.moodish_projects),
     path('moodish/resources/',views.moodish_resources),
     path('moodish/roadmap/',views.moodish_roadmap),
     path('moodish/tasks/',views.moodish_tasks),
+
+    path('runner/overview/',views.runner_overview),
+    path('runner/projects/',views.runner_projects),
+    path('runner/resources/',views.runner_resources),
+    path('runner/roadmap/',views.runner_roadmap),
+    path('runner/tasks/',views.runner_tasks),
+
+    path('marketing/overview/',views.marketing_overview),
+    path('marketing/seo/',views.marketing_seo),
+    path('marketing/resources/',views.marketing_resources),
+    path('marketing/roadmap/',views.marketing_roadmap),
+    path('marketing/social_design_templates/',views.marketing_social_design_templates),
+    path('marketing/social_moodish/',views.marketing_social_moodish),
+    path('marketing/social_snapinsight/',views.marketing_social_snapinsight),
+    path('marketing/email_homepage/',views.marketing_email_homepage),
+    path('marketing/email_list/',views.marketing_email_list),
 ]
--- OngoingProjects/SnapinsightAdmin/snapinsight/views.py
+++ OngoingProjects/SnapinsightAdmin/snapinsight/views.py
@@ -39,4 +39,54 @@ def moodish_roadmap(request):
 
 def moodish_tasks(request):
     return render(request, 'snapinsight/moodish/tasks.html')
+
+
+def runner_overview(request):
+    return render(request, 'snapinsight/runner/overview.html')
+
+def runner_projects(request):
+    return render(request, 'snapinsight/runner/projects.html')
+
+def runner_resources(request):
+    return render(request, 'snapinsight/runner/resources.html')
+
+def runner_resources(request):
+    return render(request, 'snapinsight/runner/resources.html')
+
+def runner_roadmap(request):
+    return render(request, 'snapinsight/runner/roadmap.html')
+
+def runner_tasks(request):
+    return render(request, 'snapinsight/runner/tasks.html')
+
+
+def marketing_overview(request):
+    return render(request, 'snapinsight/marketing/overview.html')
+
+def marketing_seo(request):
+    return render(request, 'snapinsight/marketing/seo.html')
+
+def marketing_resources(request):
+    return render(request, 'snapinsight/marketing/resources.html')
+
+def marketing_resources(request):
+    return render(request, 'snapinsight/marketing/resources.html')
+
+def marketing_roadmap(request):
+    return render(request, 'snapinsight/marketing/roadmap.html')
+
+def marketing_social_design_templates(request):
+    return render(request, 'snapinsight/marketing/social-design-templates.html')
+
+def marketing_social_moodish(request):
+    return render(request, 'snapinsight/marketing/social-moodish.html')
+
+def marketing_social_snapinsight(request):
+    return render(request, 'snapinsight/marketing/social-snapinsight.html')
+
+def marketing_email_homepage(request):
+    return render(request, 'snapinsight/marketing/email-homepage.html')
+
+def marketing_email_list(request):
+    return render(request, 'snapinsight/marketing/email_list.html')
 # Create your views here.
