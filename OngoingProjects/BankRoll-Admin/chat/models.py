65cd2904f2707aee6e2de64dbc307c7abbb036d3
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2020-08-14 17:34:17 +0530

    login

--- OngoingProjects/BankRoll-Admin/chat/models.py
+++ OngoingProjects/BankRoll-Admin/chat/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Room(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500)
+    password = models.CharField(max_length=500)
 # Create your models here.
--- OngoingProjects/BankRoll-Admin/chat/views.py
+++ OngoingProjects/BankRoll-Admin/chat/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import *
+from .models import *
 
 def index(request):
+    if request.method == "POST":
+        lf = LoginForm(request.POST)
+        # print(lf, lf.is_valid())
+        if lf.is_valid():
+            uname = lf.cleaned_data["uname"]
+            rname = lf.cleaned_data["rname"]
+            password = lf.cleaned_data["password"]
+            auth = Room.objects.filter(name=rname, password=password)
+            if auth.count() == 1:
+                request.session['id'] = auth[0].id
+                return HttpResponseRedirect('/chat/' + rname + '/')
+            error = "Invalid Room Name or password"
+            return render(request, 'chat/index.html', {'error':error})
+
+    lf = LoginForm()
     return render(request, 'chat/index.html')
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+    if 'id' in request.session:
+        return render(request, 'chat/room.html', {
+            'room_name': room_name
+            })
+    return render(request, 'chat/index.html')
 # Create your views here.
